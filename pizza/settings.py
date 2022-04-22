dc1ea79b3f49629e138feb9395c20b70d1b3827a
pritam_cloud_shell <pritam.vishwakarma.scrobits@gmail.com>
2022-04-22 06:03:39 +0000

    installed jwt

--- pizza/settings.py
+++ pizza/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
     'orders',
     # 3rd party
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 # auth user model settings
@@ -55,7 +57,16 @@ AUTH_USER_MODEL = 'authentication.User'
 
 
 REST_FRAMEWORK = {
-    'NON_FIELD_ERRORS_KEY'='errors'
+    'NON_FIELD_ERRORS_KEY'='errors',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    # 'AUTH_HEADER_TYPES':('Bearer',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
 
